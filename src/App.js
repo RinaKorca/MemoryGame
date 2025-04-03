a9ef466f8f9b7587475c29e6fd3ea17a86be78cf
Rina-Korca <rina.korcaa@gmail.com>
2025-04-03 16:17:41 +0200

    Add image array

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import "./App.css";
 
+const cardImg = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
 function App() {
   return (
     <div className="App">
