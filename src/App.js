449c52e9ada45b35eafda4858ec7e7d1eae5a34a
Rina-Korca <rina.korcaa@gmail.com>
2025-04-03 16:30:15 +0200

    Added the cards front and back

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 
 const cardImg = [
   { src: "/img/helmet-1.png" },
@@ -8,11 +9,35 @@ const cardImg = [
   { src: "/img/shield-1.png" },
   { src: "/img/sword-1.png" },
 ];
+
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImg, ...cardImg]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <div className="card" key={card.id}>
+            <div>
+              <img src={card.src} alt="card front" className="front" />
+              <img className="back" alt="card back" src="/img/cover.png" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
